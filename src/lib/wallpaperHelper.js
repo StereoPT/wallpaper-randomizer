e90642b7486a6c647ecdfdd3c4f14a86b8225d4b
Guido <stereopt@gmail.com>
2023-03-13 11:01:28 +0000

    refactor: subreddit name display

--- src/lib/wallpaperHelper.js
+++ src/lib/wallpaperHelper.js
@@ -5,6 +5,7 @@ import { oraPromise as ora } from 'ora';
 import path from 'node:path';
 import random from 'random';
 import axios from 'axios';
+import { capitalize } from './utils.js';
 
 import {
   WALLPAPERS_SUBREDDIT,
@@ -17,10 +18,10 @@ export const getWallpapers = async() => {
   let wallpapers = [];
   const todayDate = new Date().toLocaleDateString('pt-PT').replaceAll('/', '_');
 
-  const chooseSubreddit = random.choice(WALLPAPERS_SUBREDDIT);
+  const subreddit = random.choice(WALLPAPERS_SUBREDDIT);
   const {
     data: { data: { children: subredditPosts }}
-  } = await ora(axios.get(chooseSubreddit), ORA_OPTIONS);
+  } = await ora(axios.get(`http://www.reddit.com/r/${subreddit}/new.json?sort=new`), ORA_OPTIONS);
 
   for(const { data: post } of subredditPosts) {
     const imageNameExt = post.url.split('/').pop();
@@ -37,7 +38,8 @@ export const getWallpapers = async() => {
       url: post.url,
       imageName: newImageName,
       imageDestination: path.join(WALLPAPERS_PATH, newImageName),
-      date: todayDate
+      date: todayDate,
+      subreddit: subreddit
     });
   }
 
@@ -48,6 +50,7 @@ export const downloadRandomWallpaper = async(wallpapers) => {
   const randomWallpaper = random.choice(wallpapers);
   console.log(` - Current Wallpaper: ${randomWallpaper.title}`);
   console.log(` - Wallpaper Link: ${randomWallpaper.url}`);
+  console.log(` - From: ${capitalize(randomWallpaper.subreddit)}`);
   
   await imageDownloader.image({
     url: randomWallpaper.url, dest: randomWallpaper.imageDestination
